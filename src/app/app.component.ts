cc9aeb985b627a43597e7a01202ac0567c2b5a04
gmorenal <gmorenal@emeal.nttdata.com>
2022-05-27 10:35:16 +0200

    version inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',// nombre del componente
+  templateUrl: 'app.component.html',//parte visual de angular. Se puede escribir aquí el html o mandarlo a un fichero aparte
+  //styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+}
---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'app-contador',
+    template: `
+
+
+        <h1>{{ titulo }}</h1>
+        <h3>La base es <strong>{{ base }}</strong></h3>
+        
+        <button (click) =" operar(base) "> + {{ base }} </button>
+        
+        <span> {{ numero }} </span>
+        
+        <button (click) =" operar(-base); "> - {{ base }} </button>
+    
+    
+        `
+
+})
+export class ContadorComponent{
+
+    titulo:string = 'bases';
+    numero:number = 10;
+    base:number = 5;
+  
+    operar(valor: number){
+      this.numero +=valor;
+    }
+  
+
+}
